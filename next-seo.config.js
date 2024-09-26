d56fa1b4680ce0120f66a75f024689b80cd2148f
Abhishek <jangal&mangal.gov>
2024-09-26 14:54:18 +0530

    OpenGraph Image

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,16 +3,16 @@ export default {
   title: "ArunRaj Online Services",
   description:
     "Get all your online services like PAN card, Aadhar, Passport, EPFO, and more at affordable rates with ArunRajOnlineServices. Rated 5 stars by over 100 customers!",
-  canonical: "https://www.arunrajonlineservices.com",
+  canonical: "https://arun-raj-online-services.vercel.app",
   openGraph: {
-    url: "https://www.arunrajonlineservices.com",
+    url: "https://arun-raj-online-services.vercel.app",
     title:
       "ArunRajOnlineServices - PAN Card, Aadhar, Passport, and EPFO Services",
     description:
       "Affordable and reliable online services for PAN card, Aadhar card, Passport assistance, and EPFO services.",
     images: [
       {
-        url: "http://localhost:3000/opengraph-image.jpg",
+        url: "https://arun-raj-online-services.vercel.app/opengraph-image.jpg",
         width: 1200,
         height: 630,
         alt: "ArunRaj Online Services Banner",
